93f54c6eef6198b7030a8bca59cfea16cb960344
Emanuele Borghini <manuborg2002@gmail.com>
2022-03-07 10:32:39 +0100

    changed indentation, variables and functions names, line length and more to adhere PEP 8 style guide (https://www.python.org/dev/peps/pep-0008/)

--- main.pyw
+++ main.pyw
@@ -1,7 +1,8 @@
-from urban_dictionary import UrbanDictionary
 import PySimpleGUI as sg
 
-#urban dictionary init
+from urban_dictionary import UrbanDictionary
+
+# urban dictionary init
 ud = UrbanDictionary()
 
 # GUI layout
@@ -9,64 +10,60 @@ sg.theme("Dark Blue 3")
 layout = [
     [sg.Multiline(key="query", size=(101,1), tooltip="Type any word here...", pad=((5,5),(5,15))),
      sg.Button("🔎", key="search", size=(6,2), font=(None,11), tooltip="Search", pad=((10,5),(0,7))),
-     sg.Button("🔁",key="random", size=(6,2), font=(None,11), tooltip="Random word", pad=((5,5),(0,7)))],
-
-    [sg.Button("<<", key="previousPage", tooltip="previous page", size=(20,1), disabled = not ud.hasPreviousPage),
-     sg.Button("<", key="previousWord", tooltip="previous word", size=(20,1), disabled = not ud.hasPreviousWord),
-     sg.Button("⌂", key="homePage", tooltip="home page", size=(20,1), focus=True),
-     sg.Button(">", key="nextWord", tooltip="next word", size=(20,1), disabled = not ud.hasNextWord),
-     sg.Button(">>", key="nextPage", tooltip="next page", size=(20,1), disabled = not ud.hasNextPage),
+     sg.Button("🔁",key="random", size=(6,2), font=(None,11), tooltip="Random word", pad=((5,5),(0,7)))
     ],
-
-    [sg.Text(ud.currentWord["name"],key="name",font=(None,12,"bold"),size=(50,None), tooltip="word")],
-    [sg.Text(ud.currentWord["meaning"],key="meaning",size=(100,None), tooltip="meaning")],
-    [sg.Text(ud.currentWord["example"],key="example",font=(None,10,"italic"),size=(100,None), tooltip="example")],
-    [sg.Text(ud.currentWord["contributor"],key="contributor",size=(100,None), tooltip="contributor", pad=((5,5),(5,15)))]
+    [sg.Button("<<", key="previousPage", tooltip="previous page", size=(20,1), disabled = not ud.has_previous_page),
+     sg.Button("<", key="previousWord", tooltip="previous word", size=(20,1), disabled = not ud.has_previous_word),
+     sg.Button("⌂", key="homePage", tooltip="home page", size=(20,1), focus=True),
+     sg.Button(">", key="nextWord", tooltip="next word", size=(20,1), disabled = not ud.has_next_word),
+     sg.Button(">>", key="nextPage", tooltip="next page", size=(20,1), disabled = not ud.has_next_page),
+    ],
+    [sg.Text(ud.current_word["name"],key="name",font=(None,12,"bold"),size=(50,None), tooltip="word")],
+    [sg.Text(ud.current_word["meaning"],key="meaning",size=(100,None), tooltip="meaning")],
+    [sg.Text(ud.current_word["example"],key="example",font=(None,10,"italic"),size=(100,None), tooltip="example")],
+    [sg.Text(ud.current_word["contributor"],key="contributor",size=(100,None), tooltip="contributor", pad=((5,5),(5,15)))]
 ]
 window = sg.Window('Urban Dictionary', layout, finalize=True)
 
-#gui logics
+# gui logics
 while True:
     event, values = window.read()
 
+    # manage events
     if event is None:
         break
-
-    elif event == "homePage":
-        ud = UrbanDictionary()
-        window["query"].update("")
-
     elif event == "random":
         ud = UrbanDictionary(random=True)
         window["query"].update("")
-
     elif event == "search":
         if values.get("query").replace("\n"," "):
             query = UrbanDictionary(values.get("query").replace("\n"," "))
-            if query.currentWord:
+            if query.current_word:
                 ud = query
             else:
                 sg.popup("Sorry, we couldn't find: " + values.get('query').replace('\n', ' '), title="¯\\_(ツ)_/¯")
         else:
             sg.popup(f"Insert some text first", title="Error")
             continue
-
     elif event == "previousPage":
-        ud.goToPreviousPage()
+        ud.go_to_previous_page()
     elif event == "previousWord":
-        ud.goToPreviousWord()
-    elif event == "nextPage":
-        ud.goToNextPage()
+        ud.go_to_previous_word()
+    elif event == "homePage":
+        ud = UrbanDictionary()
+        window["query"].update("")
     elif event == "nextWord":
-        ud.goToNextWord()
+        ud.go_to_next_word()
+    elif event == "nextPage":
+        ud.go_to_next_page()
 
-    word = ud.currentWord
+    # update UI elements
+    word = ud.current_word
     window["name"].update(word["name"])
     window["meaning"].update(word["meaning"])
     window["example"].update(word["example"])
     window["contributor"].update(word["contributor"])
-
-    window["previousPage"].update(disabled = not ud.hasPreviousPage)
-    window["previousWord"].update(disabled = not ud.hasPreviousWord)
-    window["nextWord"].update(disabled = not ud.hasNextWord)
-    window["nextPage"].update(disabled = not ud.hasNextPage)
+    window["previousPage"].update(disabled = not ud.has_previous_page)
+    window["previousWord"].update(disabled = not ud.has_previous_word)
+    window["nextWord"].update(disabled = not ud.has_next_word)
+    window["nextPage"].update(disabled = not ud.has_next_page)
--- urban_dictionary.py
+++ urban_dictionary.py
@@ -1,139 +1,171 @@
-from bs4 import BeautifulSoup, element
-from requests import get
-from urllib.parse import quote
+"""TODO Docstring
+soon™
+"""
+
+__author__ = "Emanuele Borghini"
+__version__ = "0.1"
+
+__all__ = ["UrbanDictionary", "get_words_from_url"]
+
+
 import re
+import requests
+import urllib.parse
 
-def getWordsFromUrl(url : str):
+from bs4 import BeautifulSoup, element
+
+
+def _escape_markdown(text: str):
+    def replacement(match):
+        return match.groupdict().get("url") or "\\".join(match.groupdict()["markdown"])
+    regex = r"(?P<markdown>[_\\~|\*`]|%s)" % r"^>(?:>>)?\s|\[.+\]\(.+\)"
+    return re.sub(regex, replacement, text, 0, re.MULTILINE)
+
+
+def _get_string_from_div(div, markdown : bool = False):
+    """Utility function to extract a string from a div.
+
+        The string can be formatted using markdown,
+        this function is highly dependend from
+        https://www.urbandictionary.com/ html page
     """
-        Function that returns a dictionary containing the words from any valid url from https://www.urbandictionary.com/ domain
+    string = ""
+    for html_elem in (content for content in div.contents):
+        if markdown:
+            text = _escape_markdown(html_elem.text)
+        else:
+            text = html_elem.text
+
+        if isinstance(html_elem, element.Tag) and html_elem.name == "br":
+            string += "\n"
+        elif isinstance(html_elem, element.Tag) and html_elem.name == "a" and markdown:
+            string += (f"[{text}]" +
+                       f"(https://www.urbandictionary.com{html_elem.attrs['href']})")
+        else:
+            string += text
+    return string
+
+
+def get_words_from_url(url: str, markdown: bool = False):
+    """Function that returns a list containing the words from any
+       valid url from https://www.urbandictionary.com/ domain
     """
-    def getStringFromDiv(div, markdown : bool = False):
-        """
-            Utility function to extract a string from a div, the string can be formatted using markdown,
-            this function is highly dependend from https://www.urbandictionary.com/ html formatting
-        """
-        string = ""
-        for html_elem in (elem for elem in div.contents):
-            if markdown:
-                replace_func = lambda match: match.groupdict().get('url') or ('\\' + match.groupdict()['markdown'])
-                escape_markdown_regex = r'(?P<markdown>[_\\~|\*`]|%s)' % r'^>(?:>>)?\s|\[.+\]\(.+\)'
-                text = re.sub(escape_markdown_regex, replace_func, html_elem.text, 0, re.MULTILINE)
-            else:
-                text = html_elem.text
 
-            if isinstance(html_elem, element.Tag) and html_elem.name == 'br':
-                string += "\n"
-            elif isinstance(html_elem, element.Tag) and html_elem.name == "a" and markdown:
-                string += "["+text+"](https://www.urbandictionary.com"+html_elem.attrs["href"]+")"
-            else:
-                string += text
-        return string
+    # Fetch all of the html divs containing the words definitions
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    divs = soup.findAll("div", class_="definition")
 
-    #fetch all of the html divs containing the words definitions
-    divs = BeautifulSoup(get(url).content, "html.parser").findAll("div", class_='definition')
-
-    #for each div extract the informations about the word and put it in the dictionary
-    words = {}
+    # For each div extract the informations and put it in the list
+    words = []
     for contents in (div.contents[0].contents for div in divs):
-        words[len(words)] = { "url": "https://www.urbandictionary.com"+contents[0].contents[0].contents[0].attrs["href"],
-                              "name": getStringFromDiv(contents[0]),
-                              "name_markdown": getStringFromDiv(contents[0], markdown=True),
-                              "meaning": getStringFromDiv(contents[1]),
-                              "meaning_markdown": getStringFromDiv(contents[1], markdown=True),
-                              "example": getStringFromDiv(contents[2]),
-                              "example_markdown": getStringFromDiv(contents[2], markdown=True),
-                              "contributor": getStringFromDiv(contents[3]),
-                              "contributor_markdown": getStringFromDiv(contents[3], markdown=True) }
+        words.append({"url": "https://www.urbandictionary.com" +
+                             contents[0].contents[0].contents[0].attrs["href"],
+                      "name": _get_string_from_div(contents[0], markdown),
+                      "meaning": _get_string_from_div(contents[1], markdown),
+                      "example": _get_string_from_div(contents[2], markdown),
+                      "contributor": _get_string_from_div(contents[3], markdown)})
     return words
 
 
 class UrbanDictionary():
-    """a class to handle a bidirectional search query on https://www.urbandictionary.com/"""
+    """A class to handle queries on https://www.urbandictionary.com/"""
 
-    def __init__(self, query : str = None, random : bool = False, enableCaching : bool = True):
-        self.query = quote(query.strip()) if query and query.strip() else None
-        self.random = random if not self.query else False
-
-        self.wordIndex = 0
-        self.pageIndex = 1
-        self.words = getWordsFromUrl(self.url)
-
-        self.cachingEnabled = enableCaching
-        self.pages = {self.pageIndex: self.words} if self.cachingEnabled else {}
+    def __init__(self,
+                query: str = None,
+                random: bool = False,
+                markdown: bool = False,
+                caching: bool = True):
+        if query and query.strip():
+            self.query = urllib.parse.quote(query.strip())
+            self.random = False
+        else:
+            self.query = None
+            self.random = random
+        self.is_markdown_enabled = markdown
+        self.is_caching_enabled = caching
+        self.word_index = 0
+        self.page_index = 1
+        self.page = get_words_from_url(self.url, self.is_markdown_enabled)
+        self.pages = {self.page_index: self.page} if self.is_caching_enabled else {}
 
     @property
     def url(self):
-        string = "https://www.urbandictionary.com/"
+        url = "https://www.urbandictionary.com/"
         if self.query:
-            string += f"define.php?term={self.query}&"
+            url += f"define.php?term={self.query}&"
         elif self.random:
-            string += f"random.php?"
+            url += "random.php?"
         else:
-            string += "?"
-        string += f"page={self.pageIndex}"
-        return string
+            url += "?"
+        return url + f"page={self.page_index}"
 
     @property
-    def currentWord(self):
-        return self.words.get(self.wordIndex)
+    def current_word(self):
+        if self.page:
+            return self.page[self.word_index]
+        else:
+            return None
 
     @property
-    def hasPreviousPage(self):
-        return self.random or self.pageIndex > 1
+    def has_previous_page(self):
+        return self.random or self.page_index > 1
 
     @property
-    def hasNextPage(self):
-        self.pageIndex += 1
-        words = self.pages.get(self.pageIndex) or getWordsFromUrl(self.url)
-        if self.cachingEnabled: self.pages[self.pageIndex] = words
-        self.pageIndex -= 1
+    def has_previous_word(self):
+        return self.word_index > 0 or self.has_previous_page
+
+    @property
+    def has_next_page(self):
+        self.page_index += 1
+        words = (self.pages.get(self.page_index) or
+                 get_words_from_url(self.url, self.is_markdown_enabled))
+        if self.is_caching_enabled:
+            self.pages[self.page_index] = words
+        self.page_index -= 1
         return self.random or len(words) > 0
 
     @property
-    def hasPreviousWord(self):
-        return self.wordIndex > 0 or self.hasPreviousPage
+    def has_next_word(self):
+        return self.word_index < len(self.page)-1 or self.has_next_page
 
-    @property
-    def hasNextWord(self):
-        return self.wordIndex < len(self.words)-1 or self.hasNextPage
-
-    def goToPreviousPage(self):
-        if not self.hasPreviousPage:
+    def go_to_previous_page(self):
+        if not self.has_previous_page:
             raise StopIteration("There isn't a Page prior to the current one")
 
-        self.wordIndex = 0
-        self.pageIndex -= 1
-        self.words = self.pages.get(self.pageIndex) or getWordsFromUrl(self.url)
-        return self.words
+        self.word_index = 0
+        self.page_index -= 1
+        self.page = (self.pages.get(self.page_index) or
+                     get_words_from_url(self.url, self.is_markdown_enabled))
+        return self.page
 
-    def goToPreviousWord(self):
-        if not self.hasPreviousWord:
+    def go_to_previous_word(self):
+        if not self.has_previous_word:
             raise StopIteration("There isn't a Word prior to the current one")
 
-        if self.wordIndex > 0:
-            self.wordIndex -= 1
+        if self.word_index > 0:
+            self.word_index -= 1
         else:
-            self.goToPreviousPage()
-            self.wordIndex = len(self.words)-1
+            self.go_to_previous_page()
+            self.word_index = len(self.page)-1
+        return self.current_word
 
-        return self.currentWord
+    def go_to_next_page(self):
+        if not self.has_next_page:
+            raise StopIteration("There isn't a Page after the current one")
 
-    def goToNextPage(self):
-        if not self.hasNextPage:
-            raise StopIteration("There isn't a Page subsequent to the current one")
+        self.word_index = 0
+        self.page_index += 1
+        self.page = (self.pages.get(self.page_index) or
+                     get_words_from_url(self.url, self.is_markdown_enabled))
+        return self.page
 
-        self.wordIndex = 0
-        self.pageIndex += 1
-        self.words = self.pages.get(self.pageIndex) or getWordsFromUrl(self.url)
-        return self.words
+    def go_to_next_word(self):
+        if not self.has_next_word:
+            raise StopIteration("There isn't a Word after the current one")
 
-    def goToNextWord(self):
-        if not self.hasNextWord:
-            raise StopIteration("There isn't a Word subsequent to the current one")
-
-        if self.wordIndex < len(self.words)-1:
-            self.wordIndex += 1
+        if self.word_index < len(self.page)-1:
+            self.word_index += 1
         else:
-            self.goToNextPage()
-
-        return self.currentWord
+            self.go_to_next_page()
+        return self.current_word
